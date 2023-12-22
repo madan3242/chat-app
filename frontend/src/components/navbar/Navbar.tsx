41e342b598b4ddcb51146b63aedcf255d6870a68
Madan <madangowda3242@gmail.com>
2023-12-22 11:09:46 +0530

    navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
   return (
     <div className="h-16 w-full shadow-md flex items-center py-2 px-10 justify-between">
       <Link to={"/"}>
-        <h1 className="text-2xl font-bold text-slate-600">🤙 Yolo</h1>
+        <h1 className="text-2xl font-bold text-slate-600">🤙 Mingle</h1>
       </Link>
       <div>
         <Link to={"/login"}>
