3d814d6d3b5c9d3b878985357aabdc0e927302fd
Madan <madangowda3242@gmail.com>
2024-01-18 11:52:03 +0530

    models updated

--- backend/src/models/chat.models.ts
+++ backend/src/models/chat.models.ts
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  isGroupChat: { type: Boolean, default: false },
-  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+  chatName: { 
+    type: String, 
+    required: true 
+  },
+  isGroupChat: { 
+    type: Boolean, 
+    default: false 
+  },
+  lastestMessage: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Message" 
+  },
+  users: [
+    { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User" 
+    }
+  ],
+  groupAdmin: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User" 
+  },
+}, { timestamps: true }
+);
 
 export const Chat = mongoose.model("Chat", chatSchema);
--- backend/src/models/message.models.ts
+++ backend/src/models/message.models.ts
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  content: { type: String},
-  chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-  readBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  sender: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User" 
+  },
+  content: { 
+    type: String
+  },
+  chat: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Chat" 
+  },
+  readBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User" 
+  },
 }, { timestamps: true }
 );
 
