02585920d1322f4d17348cd239eb20df0c1c5c52
Madan <madangowda3242@gmail.com>
2024-01-25 21:48:57 +0530

    api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,8 +4,24 @@ const apiClient = axios.create({
     baseURL: import.meta.env.BASE_URL,
     withCredentials: true,
     timeout: 1200000
-})
+});
 
-export const loginUser = () => {
-    return apiClient.get("/api/v1/auth/login")
+apiClient.interceptors.request.use(
+  function (config) {
+    const token = localStorage.get("token");
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+type loginData = {
+    username?: string,
+    password?: string
 }
+
+export const loginUser = (data: loginData) => {
+    return apiClient.get("/api/v1/auth/login", data)
+};
